d164b246219db8f020b4ed639505b01072b6e29d
kohei_ishikawa@iwi.co.jp <kohei_ishikawa@iwi.co.jp>
2020-01-16 14:36:49 +0900

    固定長ファイルを読んでPersonクラスにデシリアライズし、デシリアライズしたPersonクラスのToStringをログ出力する

---
+++ FixedFileReadToLoggerWrite/src/main/java/com/example/filetologger/Person.java
@@ -0,0 +1,45 @@
+package com.example.filetologger;
+
+public class Person {
+
+	private String id;
+	private String name;
+	private String birthday;
+	private String gender;
+
+	public Person() {
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getBirthday() {
+		return birthday;
+	}
+
+	public void setBirthday(String birthday) {
+		this.birthday = birthday;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+}
